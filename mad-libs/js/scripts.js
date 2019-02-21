ad0acce17a50f27e66a1163ebf76f4708e54480d
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-21 13:18:31 +0300

    added files

---
+++ mad-libs/js/scripts.js
@@ -0,0 +1,42 @@
+// $(document).ready(function() {
+//     $("#blanks form").submit(function() {
+//         $(".person1").append("blah blah");
+//         $(".person2").append("blah blah");
+//         $(".animal").append("blah blah");
+//         $(".exclamation").append("blah blah");
+//         $(".verb").append("blah blah");
+//         $(".noun").append("blah blah");
+//
+//         $("#story").show();
+//     });
+// });
+// $(document).ready(function() {
+//     $("#blanks form").submit(function(event) {
+//         $(".person1").append("Maurice");
+//         $(".person2").append("blah blah");
+//         $(".animal").append("blah blah");
+//         $(".exclamation").append("blah blah");
+//         $(".verb").append("blah blah");
+//         $(".noun").append("blah blah");
+//
+//         $("#story").show();
+//
+//         event.preventDefault();
+//     });
+// });
+
+
+
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
